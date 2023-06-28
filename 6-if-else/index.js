d8449015cd04d47839fac860732f0ab2f3cc9631
seraykosem <132205424+seraykosem@users.noreply.github.com>
2023-06-28 08:51:32 +0300

    Add files via upload

---
+++ 6-if-else/index.js
@@ -0,0 +1,25 @@
+const username = "seraykosem";
+const password = 123456;
+
+
+if(username == "seraykosem"){
+    if(password === 123456){
+        console.log("Giriş Yapıldı.");
+    }else{
+        console.log("Şifre Yanlış!");
+    }
+}else{
+    console.log("Kullanıcı Bulunamadı.");
+}
+
+
+
+
+/* 
+let isLoggedIn = false;
+if(isLoggedIn == true){
+    console.log("çalıştı.");
+}else{
+    console.log("true değil");
+}
+console.log("Çıktı!"); */
